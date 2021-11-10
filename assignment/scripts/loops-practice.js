289a585e0af25aba7b2e93fb1115e2f4c052111c
Sabrina Ciaciura <ciaci0028@gmail.com>
2021-11-10 15:58:46 -0600

    middle push

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -22,14 +22,27 @@ for (let i=0; i<6; i++) {
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
+
+// You have to change the initial i variable starting point.
 console.log('count from 3 to 5');
+for (let i=3; i<6; i++){
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
+
+// You have to increment it by 2 instead of 1 (which is represented by ++)
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for (let i=2; i<11; i=i+2){
+  console.log(i);
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
+for (let i=5; i>-1; i--){
+  console.log(i);
+}
 
 
 // 2. For of loops
@@ -38,7 +51,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for ( star of stars ){
+  console.log( 'the star is called', star );
+}
 
 // 3. While loops
 console.log('---- 3. While loop ----');
